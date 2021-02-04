2490e9403d605fd0774569b75bf436dc584df7d6
KROSY <45696255+krosy1337@users.noreply.github.com>
2021-02-04 19:42:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,39 +11,79 @@ const controlButtonHandler = e => {
     const target = e.target
 
     if (target.classList.contains('control__button')) {
-        random = Math.round(Math.random()*100)
-        smallBall.textContent = random
-        if (target.classList.contains('more')) {
-            if (+smallBall.textContent > +bigBall.textContent) {
-                result.textContent = "Победа"
-                result.style.color = '#6bb560'
+
+        const sP = new Promise((resolve, reject) => {
+            let j = 0
+            secondInterval = setInterval(() => {
+                if (j === 100) {
+                    clearInterval(secondInterval)
+                    resolve()
+                }
+                smallBall.textContent = Math.floor(Math.random() * (98 - 2 + 1)) + 2
+                j++
+            }, 10)
+        })
+
+        sP.then(() => {
+            random = Math.floor(Math.random() * (100 - 1+ 1)) + 1
+            smallBall.textContent = random
+            if (target.classList.contains('more')) {
+                if (+smallBall.textContent > +bigBall.textContent) {
+                    result.textContent = "Победа"
+                    result.style.color = '#6bb560'
+                } else {
+                    result.textContent = "Поражение"
+                    result.style.color = '#985151'
+                }
+            } else if (target.classList.contains('less')){
+                if (+smallBall.textContent < +bigBall.textContent) {
+                    result.textContent = "Победа"
+                    result.style.color = '#6bb560'
+                } else {
+                    result.textContent = "Поражение"
+                    result.style.color = '#985151'
+                }
             } else {
-                result.textContent = "Поражение"
-                result.style.color = '#985151'
+                if (+smallBall.textContent === +bigBall.textContent) {
+                    result.textContent = "Победа"
+                    result.style.color = '#6bb560'
+                } else {
+                    result.textContent = "Поражение"
+                    result.style.color = '#985151'
+                }
             }
-        } else {
-            if (+smallBall.textContent < +bigBall.textContent) {
-                result.textContent = "Победа"
-                result.style.color = '#6bb560'
-            } else {
-                result.textContent = "Поражение"
-                result.style.color = '#985151'
-            }
-        }
+        })
     }
 
     control.removeEventListener('click', controlButtonHandler)
 }
 
-startButton.addEventListener('click', () => {
-    random = Math.round(Math.random()*100)
 
-    bigBall.textContent = random
+startButton.addEventListener('click', () => {
+    random = Math.floor(Math.random() * (98 - 2 + 1)) + 2
 
     smallBall.textContent = ''
     result.textContent = ''
 
-    control.addEventListener('click', controlButtonHandler)
-})
+    const p = new Promise((resolve, reject) => {
+        let i = 0
+        let interval = setInterval(() => {
+            if (i === 100) {
+                clearInterval(interval)
+                resolve()
+            }
+            bigBall.textContent = Math.floor(Math.random() * (98 - 2 + 1)) + 2
+            i++
+        }, 10)
+    })
 
+
+    p.then(() => {
+        bigBall.textContent = random
+
+        control.addEventListener('click', controlButtonHandler)
+    })
+
+
+})
 
